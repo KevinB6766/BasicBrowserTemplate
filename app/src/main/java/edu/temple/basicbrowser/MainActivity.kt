79a7152d0a0ec009b1eccb4937534742547ae81c
KevinB6766 <tuu21653@temple.edu>
2025-03-27 12:10:38 -0400

    Added onclick listener to gobutton to load url to webview after checking if it has the proper protocols at the beginning. If not, then they will be appended. Also enable javascript and internet access

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -21,12 +21,28 @@ class MainActivity : AppCompatActivity() {
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
 
+        webView.settings.javaScriptEnabled = true // Enable JavaScript if needed
+
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
             }
         }
+
+        goButton.setOnClickListener {
+            val url = urlEditText.text.toString().trim() // Remove leading/trailing spaces
+            if (url.isNotEmpty()){ // check if url is not empty
+                if (!url.startsWith("http://") && !url.startsWith("https://")){ // check if url starts with http or https
+                    urlEditText.setText("https://$url") // add https:// to the url
+                }
+            }
+            webView.loadUrl(url) // load the url in the webview
+        }
+        //use this in android manifest xml in outside application and activity tags
+        //<uses-permission android:name="android.permission.INTERNET"/>
+        //allows access to the internet
+
 
     }
 }
